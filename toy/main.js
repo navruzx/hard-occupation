2c592e09556bb08ec2856c39f3e8d85cb93b0652
navruzx <nnavruz095@gmail.com>
2024-08-07 17:44:04 +0500

    Add files via upload

---
+++ toy/main.js
@@ -0,0 +1,94 @@
+// class Worker {
+//  constructor(name,familiya,age,kasb,status,manzil){
+//     this.name = name;
+//     this.familiya = familiya;
+//     this.age = age;
+//     this.kasb = kasb;
+//     this.status = status;
+//     this.manzil = manzil;
+//  }
+//  work(){
+//     console.log(`Men ${this.job} bo'lib ishlayman ${this.manzil}`);
+    
+//  }
+//  getInf(){
+//     console.log(`Mening ismim ${this.name} familiyam ${this.familiya} yoshim ${this.age} kasbim ${this.kasb} statusim ${this.status} manzilim ${this.manzil}`);
+//  }
+// }
+// let ishchi = new Worker("Aziz","Azizov",23,"Dasturchi","Young","Meta");
+// let ishchi1 = new Worker("Aziza","Azizova",22,"Advokat","Juda baland","Samarqand");
+// console.log(ishchi1);
+// class Animal {
+//     constructor(type,age){
+//         this.age = age;
+//         this.type = type;
+//     }
+// }
+// class Kvadrat {
+//     constructor(a) {
+//       this.a = a;
+//     }
+//     peremeter() {
+//       let P = 4 * this.a;
+//       console.log(P);
+//     }
+//   }
+//   let kv = new Kvadrat(4);
+//   // kv.peremeter();
+//   class KV {
+//     kvadrat(a, b) {
+//       console.log(a + b);
+//     }
+//   }
+//   let k = new KV();
+//   k.kvadrat(4, 5);
+// class Kvadrat {
+//     constructor(a) {
+//       this.a = a;
+//     }
+// //     peremeter() {
+// //       let P = 4 / this.a;
+// //       console.log(P);
+// //     }
+// //   }
+// //   let kv = new Kvadrat(4);
+// //   // kv.peremeter();
+// //   class KV {
+// //     kvadrat(a, b) {
+// //       console.log(a + b);
+// //     }
+// //   }
+// //   let k = new KV();
+// //   k.kvadrat(4, 5);
+// class Turt {
+//     constructor(a, b) {
+//       this.a = a;
+//       this.b = b;
+//     }
+//     yuza() {
+//       let S = this.a * this.b;
+//       console.log(S);
+//     }
+//     peremter() {
+//       let p = 2 * (this.a +this.b);
+//       console.log(p);
+//     }
+//   }
+//   let t = new Turt(3, 4);
+//   t.peremter();
+//   t.yuza();
+
+//   class Location{
+//     constructor(p,d) {
+//         this.d = d;
+//         this.p = p;
+//     }
+//     uzunlig(){
+//         let U = this.d * this.p;
+//         console.log(U);
+//     }
+//   }
+//   let L = new Location(5,6);
+// L.uzunlig()
+
+  
